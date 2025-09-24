27b14754d609578d7f07aa4f373e439dbaf33507
samuelcromwell <Cromwellsamuel3@gmail.com>
2025-09-24 13:21:15 +0300

    perf: preload hero images & enable eager loading for faster hero slider render

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+// pages/_document.js
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
@@ -5,13 +6,25 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Poppins:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+
+          {/* Favicon */}
           <link rel="icon" href="/Logo/logovector.svg" />
+
+          {/* 🚀 Preload ALL hero images so they begin downloading before paint */}
+          <link rel="preload" as="image" href="/images/hero/smart-season.png" />
+          <link rel="preload" as="image" href="/images/hero/cashboost.jpg" />
+          <link rel="preload" as="image" href="/images/hero/vet-care.jpg" />
+          <link rel="preload" as="image" href="/images/hero/shamba-shield.jpg" />
+          <link rel="preload" as="image" href="/images/hero/shamba-connect.jpg" />
+          {/* (Optional) add type= for slight hinting:
+              type="image/png" / type="image/jpeg" */}
         </Head>
         <body>
           <Main />
